df1325cec45daecb1afd4451377a745cb62d07f3
Patrick Hobusch <patrick.hobusch@gmail.com>
2021-11-10 12:47:18 +0800

    Remove unneeded script entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Install Helm plugins
-
-helm plugin install https://github.com/databus23/helm-diff --version 3.1.3
-helm plugin install https://github.com/aslafy-z/helm-git --version 0.10.0
-
-exec "${@}"
